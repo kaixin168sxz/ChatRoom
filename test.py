8bc465d4739c5331922cc89f3f0c3841bc90bcd5
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-04-11 20:17:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-from nicegui import ui
+import hashlib
 
-ui.query('body').classes('bg-blue')
+def md5_encrypt(data):
+    md5 = hashlib.md5()
+    md5.update(data.encode('utf-8'))
+    return md5.hexdigest()
 
-ui.run()
+# 测试示例
+# data = "你好"
+# encrypted_data = md5_encrypt(data)
+# print("加密前的数据：", data)
+# print("加密后的数据：", encrypted_data)
+
+data = "哈哈"
+encrypted_data = md5_encrypt(data)
+print("加密前的数据：", data)
+print("加密后的数据：", encrypted_data)
