ef2479e67292df51fed57d5c6fe1646bdf5ea50e
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-04-11 20:17:15 +0800

    正常登陆后，可以发送消息

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import Request
 from fastapi.responses import RedirectResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from nicegui import Client, app, ui
+from datetime import datetime
+from typing import List, Tuple
+from uuid import uuid4
 
 unrestricted_page_routes = {'/login'}
+messages: List[Tuple[str, str, str, str]] = []
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
@@ -38,14 +42,45 @@ def login():
         with ui.button('登录', on_click=try_login).classes('x-center').props('rounded outline'):
             ui.tooltip('登录到聊天室').classes('bg-green')
 
+@ui.refreshable
+def chat_messages(own_id: str) -> None:
+    for user_id, avatar, text, stamp in messages:
+        ui.chat_message(text=text, stamp=stamp, avatar=avatar, sent=own_id == user_id, name=app.storage.user.get('username'))
+    ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)')
+
 @ui.page('/')
 async def main():
     if not app.storage.user.get('authenticated', False):
         return RedirectResponse('/login')
-    ui.label(app.storage.user.get('username'))
+
     def logout():
         app.storage.user.clear()
         ui.navigate.to(app.storage.user.get('referrer_path', '/'))
+
+    ui.label(app.storage.user.get('username'))
     ui.button('注销', on_click=logout)
+
+    def send() -> None:
+        stamp = datetime.utcnow().strftime('%X')
+        messages.append((user_id, avatar, text.value, stamp))
+        text.value = ''
+        chat_messages.refresh()
+
+    user_id = str(uuid4())
+    avatar = f'https://robohash.org/{user_id}?bgset=bg2'
+
+    ui.add_css(r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}')
+    with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
+        with ui.row().classes('w-full no-wrap items-center'):
+            with ui.avatar().on('click', lambda: ui.navigate.to(main)):
+                ui.image(avatar)
+            text = ui.input(placeholder='message').on('keydown.enter', send) \
+                .props('rounded outlined input-class=mx-3').classes('flex-grow')
+        ui.markdown('simple chat app built with [NiceGUI](https://nicegui.io)') \
+            .classes('text-xs self-end mr-8 m-[-1em] text-primary')
+
+    await ui.context.client.connected()  # chat_messages(...) uses run_javascript which is only possible after connecting
+    with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
+        chat_messages(user_id)
 
 ui.run(port=66, storage_secret='THIS_NEEDS_TO_BE_CHANGED')
