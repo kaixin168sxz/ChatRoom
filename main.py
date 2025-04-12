77af1e03df8aa433937db52f5e6bd3133b557b80
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-04-12 23:07:25 +0800

    修改部分网页url名称，添加注册功能

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from typing import List, Tuple
 from db import ChatRoomDB, md5_encrypt
 from settings import *
 
-unrestricted_page_routes = {'/login'}
+unrestricted_page_routes = {'/signin', '/signup'}
 messages: List[Tuple[str, str, str, str]] = []
 db = ChatRoomDB('./Database/chatroom.db')
 
@@ -16,13 +16,13 @@ class AuthMiddleware(BaseHTTPMiddleware):
         if not app.storage.user.get('authenticated', False):
             if request.url.path in Client.page_routes.values() and request.url.path not in unrestricted_page_routes:
                 app.storage.user['referrer_path'] = request.url.path
-                return RedirectResponse('/login')
+                return RedirectResponse('/signin')
         return await call_next(request)
 
 app.add_middleware(AuthMiddleware)
 
-@ui.page('/login')
-def login():
+@ui.page('/signin')
+def signin():
     ui.add_css('''
         .subtitle {
             font-size: 20px;
@@ -34,7 +34,7 @@ def login():
     ui.query('body').style(f'background-color: #ddeeff')
     if app.storage.user.get('authenticated', False):
         return RedirectResponse('/')
-    def try_login():
+    def try_signin():
         user = username.value
         passwd = password.value
         user_data = db.get_user_data(user)
@@ -60,9 +60,12 @@ def login():
     with ui.card().classes('absolute-center').style(f'background-color: #edf7ff').props('flat bordered'):
         ui.label('欢迎来到聊天室').classes('subtitle')
         username = ui.input('用户').classes('fill').props('dense outlined')
-        password = ui.input('密码', password=True).classes('inline-flex').props('dense outlined').on('keydown.enter', try_login)
-        with ui.button('登录', on_click=try_login).classes('x-center bg-green').props('size=md push'):
+        password = ui.input('密码', password=True).classes('inline-flex').props('dense outlined').on('keydown.enter', try_signin)
+        with ui.button('登录', on_click=try_signin).classes('x-center bg-green').props('size=md push'):
             ui.tooltip('欢迎来到聊天室').classes('bg-green').props('transition-show="scale" transition-hide="scale"')
+        with ui.row().classes('w-full items-center gap-2'):
+            ui.space()
+            ui.link('注册用户', signup).classes('text-xs text-green')
 
 @ui.refreshable
 def chat_messages(name) -> None:
@@ -73,9 +76,6 @@ def chat_messages(name) -> None:
 @ui.page('/')
 async def main():
     ui.add_css('''
-        .subtitle {
-            font-size: 20px;
-        }
         a:link, a:visited {
             color: inherit !important; 
             text-decoration: none; 
@@ -84,27 +84,33 @@ async def main():
         ''')
 
     if not app.storage.user.get('authenticated', False):
-        return RedirectResponse('/login')
+        return RedirectResponse('/signin')
+
+    chat_messages.refresh()
 
     def logout():
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         length_list = [len(i) + 19 for i in (user_id, now)]      # 19是信息前提示词的宽度("| User        ->   "...)
         length_list.append(len('+-----------------------------------'))
-        length_list.append(len('| INFO: user logged out successfully:'))
+        length_list.append(len('| INFO: user signed out successfully:'))
         max_length = max(length_list)
         max_length += 1
-        print('+-----------------------------------'                   + '-' * (max_length - length_list[2]) + '+\n' +
-              '| \033[1;34mINFO\033[0m: user logged out successfully:' + ' ' * (max_length - length_list[3]) + '|\n' +
-              f'| User        ->   {user_id}'                          + ' ' * (max_length - length_list[0]) + '|\n' +
-              f'| Time(Log)   ->   {now}'                              + ' ' * (max_length - length_list[1]) + '|\n' +
-              '+-----------------------------------'                   + '-' * (max_length - length_list[2]) + '+')
+        print( '+-----------------------------------'                   + '-' * (max_length - length_list[2]) + '+\n' +
+               '| \033[1;34mINFO\033[0m: user signed out successfully:' + ' ' * (max_length - length_list[3]) + '|\n' +
+              f'| User        ->   {user_id}'                           + ' ' * (max_length - length_list[0]) + '|\n' +
+              f'| Time(Log)   ->   {now}'                               + ' ' * (max_length - length_list[1]) + '|\n' +
+               '+-----------------------------------'                   + '-' * (max_length - length_list[2]) + '+')
 
         app.storage.user.clear()
-        ui.navigate.to(app.storage.user.get('referrer_path', '/'))
+        ui.navigate.to(app.storage.user.get('referrer_path', '/signin'))
 
     def send() -> None:
+        if not text.value.strip():
+            ui.notify('内容不能为空', type='info', position='top')
+            return
         stamp = datetime.now().strftime('%X')
         messages.append((user_id, avatar, text.value, stamp))
+        db.new_message(user_id, text.value)
         text.value = ''
         chat_messages.refresh()
 
@@ -127,6 +133,7 @@ async def main():
     with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
         with ui.row().classes('w-full no-wrap items-center'):
             text = ui.input('请输入消息').on('keydown.enter', send).props('dense outlined input-class=mx-3').classes('flex-grow')
+            ui.button(icon='send', text='发送', on_click=send).props('size=md push').classes('bg-green')
         with ui.row().classes('w-full no-wrap'):
             ui.space()
             ui.markdown('[Copyright © 2025 kaixin168sxz](https://github.com/kaixin168sxz/ChatRoom)').classes('text-xs self-end mr-8 m-[-1em] text-primary')
@@ -135,5 +142,46 @@ async def main():
 
     with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
         chat_messages(user_id)
+
+@ui.page('/signup')
+def signup() -> None:
+    ui.add_css('''
+        .subtitle {
+            font-size: 20px;
+        }
+        .x-center {
+            margin: auto;
+            width: 50%;
+        }''')
+    def try_signup() -> None:
+        if email.value and email.value.count('@') != 1:
+            ui.notify('邮件格式错误', type='warning', position='top')
+            return
+        if not email.value and email_switch.value:
+            ui.notify('要使用邮箱通知，请输入邮箱', type='warning', position='top')
+        try_db = db.sign_up(username.value, password.value, email.value, {True: 'EmailSend', False: 'EmailNoSend'}[email_switch.value])
+        if try_db == EXISTS:
+            ui.notify('用户名已存在', type='warning', position='top')
+            return
+        ui.notify('注册成功', type='info', position='top')
+        ui.navigate.to(app.storage.user.get('referrer_path', '/signin'))
+
+    ui.query('body').style(f'background-color: #ddeeff')
+    with ui.card().classes('absolute-center').style(f'background-color: #edf7ff').props('flat bordered'):
+        ui.label('注册用户').classes('subtitle')
+        username = ui.input('用户').classes('fill').props('dense outlined')
+        password = ui.input('密码', password=True).classes('inline-flex').props('dense outlined')
+        def update_switch_val():
+            if email.value:
+                email_switch.set_value(True)
+            else:
+                email_switch.set_value(False)
+        email = ui.input('邮箱（可选）', on_change=update_switch_val).classes('inline-flex').props('dense outlined').on('keydown.enter', try_signup)
+        email_switch = ui.switch('邮箱通知').props('icon="mail" color="green"').bind_visibility_from(email, 'value')
+        with ui.button('注册', on_click=try_signup).classes('x-center bg-green').props('size=md push'):
+            ui.tooltip('立即注册用户').classes('bg-green').props('transition-show="scale" transition-hide="scale"')
+        with ui.row().classes('w-full items-center gap-2'):
+            ui.space()
+            ui.link('登录用户', signin).classes('text-xs text-green')
 
 ui.run(port=66, storage_secret='THIS_NEEDS_TO_BE_CHANGED', language='zh-CN')
